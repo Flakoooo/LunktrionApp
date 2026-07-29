4e05099584a09a58679b785c86d6c4675088a2b3
Flakoooo <vlasov.vadom@gmail.com>
2026-07-30 02:13:20 +0500

    Add project files.

---
+++ LunktrionApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LunktrionApp.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ LunktrionApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,45 @@
+using Avalonia.Animation.Easings;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace LunktrionApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    //private void Button_OnClick(object? sender, RoutedEventArgs e)
+    //{
+    //    if (double.TryParse(Celsius.Text, out double C))
+    //    {
+    //        var F = C * (9d / 5d) + 32;
+    //        Fahrenheit.Text = F.ToString("0.0");
+    //    }
+    //    else
+    //    {
+    //        Celsius.Text = "0";
+    //        Fahrenheit.Text = "0";
+    //    }
+    //}
+
+    //private void Celsius_TextChanged(object? sender, RoutedEventArgs e)
+    //{
+    //    if (string.IsNullOrEmpty(Celsius.Text) || Celsius.Text == "-")
+    //    {
+    //        Fahrenheit.Text = "";
+    //    }
+    //    else if (double.TryParse(Celsius.Text, out double C))
+    //    {
+    //        var F = C * (9d / 5d) + 32;
+    //        Fahrenheit.Text = F.ToString("0.0");
+    //    }
+    //    else
+    //    {
+    //        Celsius.Text = "0";
+    //        Fahrenheit.Text = "0";
+    //    }
+    //}
+}
